01b54f3e1794a8d91ff0c9ea607e69a5507a7a2e
GordienkoRoman <44196911+GordienkoRoman@users.noreply.github.com>
2018-10-17 16:03:16 +0500

    Update Variant1.sql

--- Variant1.sql
+++ Variant1.sql
@@ -3,5 +3,22 @@ Select *
 from Students
 Order by groupid Asc, name desc
 2.
-select groupid,count(
+select groupid,count(*)
 from students
+group by groupid
+3.
+Select s.name,g.num 
+from students s join groups g on(s.group id=g.groupid)
+4.
+select sub.subjectid,sub.name,AVG(mark),count(ex.mark)
+from subjects sub join Exams ex on(sub.subjectid=ex.subjectid)
+where count(ex.mark)>10
+5.
+select studentid
+from students
+where studentid not in(select subjectid from exams)
+6.
+select s.name
+from students s join exams ex on(s.studentid=ex.studentid)
+group by s.name
+having min(mark)>2
